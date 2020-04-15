3929b5f05789f4a5e05b5152c519c0d689951ec0
burn-c <chopcsom@gmail.com>
2020-04-14 22:20:53 -0300

    Feacture: List transactions balance

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -24,7 +24,25 @@ class TransactionsRepository {
   }
 
   public getBalance(): Balance {
-    // TODO
+    const incomeTransactions = this.transactions.filter(
+      transaction => transaction.type === 'income',
+    );
+
+    const outcomeTransactions = this.transactions.filter(
+      transaction => transaction.type === 'outcome',
+    );
+
+    const income = incomeTransactions.reduce((prevTotal, currentValue) => {
+      return prevTotal + currentValue.value;
+    }, 0);
+
+    const outcome = outcomeTransactions.reduce((prevTotal, currentValue) => {
+      return prevTotal + currentValue.value;
+    }, 0);
+
+    const total = income - outcome;
+
+    return { income, outcome, total };
   }
 
   public create({ title, value, type }: CreateTransaction): Transaction {
